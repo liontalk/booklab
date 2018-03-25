1702d51e294dbf5a26f12f72369f96e225bbc785
liontalk <liontalk@163.com>
2018-03-25 19:03:53 +0800

    项目数据提交

---
+++ src/main/java/cn/liontalk/BooklabApplication.java
@@ -0,0 +1,12 @@
+package cn.liontalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklabApplication.class, args);
+	}
+}
