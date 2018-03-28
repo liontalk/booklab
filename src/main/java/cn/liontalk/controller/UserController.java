ffaccd5832e5cae6ce2bad7bf8dd7a1ba53ed1be
liontalk <liontalk@163.com>
2018-03-28 22:12:25 +0800

    添加表单所需要的js文件

--- src/main/java/cn/liontalk/controller/UserController.java
+++ src/main/java/cn/liontalk/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
