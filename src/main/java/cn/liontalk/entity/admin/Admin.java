101a4e7ca457d2718ebd043d8d4af86986ae5145
liontalk <liontalk@163.com>
2018-04-09 23:03:33 +0800

    添加管理员实体

--- src/main/java/cn/liontalk/entity/admin/Admin.java
+++ src/main/java/cn/liontalk/entity/admin/Admin.java
@@ -1,5 +1,12 @@
 package cn.liontalk.entity.admin;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
 /**
  * @author: 周哲
  * @package: cn.liontalk.entity.Admin
@@ -7,5 +14,23 @@ package cn.liontalk.entity.admin;
  * @date: 2018/3/25 20:32
  * @version: V1.0
  */
+@Data
+@Entity
 public class Admin {
+
+    @NonNull
+    private int id;
+
+    private String account;
+
+    private String password;
+
+    private String name;
+
+    private Date createTime;
+
+    private int parentId;
+
+    private String status;
+
 }
